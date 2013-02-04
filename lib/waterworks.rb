9f61022056b30bddcdbc797751a6c9a4c71f21ce
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-02-04 20:11:58 +0900

    add comand-line options

--- lib/waterworks.rb
+++ lib/waterworks.rb
@@ -5,11 +5,25 @@ module Waterworks
   require 'waterworks/extractor'
 
   def self.run
-    uri = ARGV.first
-    case uri
-    when /himado/
-      require 'waterworks/extractors/himado'
-      Himado.new(uri).save
+    case ARGV.first
+    when /--version$|-v$/
+      puts Waterworks::VERSION
+    when /http/
+      uri = ARGV.first
+      Extractor.matches(uri) do |extractor|
+        extractor.save(uri)
+      end
+    when /--help|-h|help/
+      usage
+    else
+      usage
     end
+  end
+
+  def self.usage
+    puts File.basename(__FILE__, '.rb')
+    puts "      <uri>         # Extract file locations from URI, and download that."
+    puts "      --help,    -h # Display help"
+    puts "      --version, -v # Display version"
   end
 end
