4c55e2c1df7b283b30501e7e46e2dc1c98c6a97f
Tomohiro TAIRA <tomohiro.t@gmail.com>
2014-09-22 23:52:50 +0900

    Remove spec

--- spec/waterworks_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Waterworks do
-  it 'should have a version number' do
-    Waterworks::VERSION.should_not be_nil
-  end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
-end
